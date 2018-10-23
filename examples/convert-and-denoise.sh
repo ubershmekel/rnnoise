0fa5058fbd07a0b4cc273bb8938b44e823a15469
Yuval Greenfield <ubershmekel@gmail.com>
2018-10-22 17:37:49 -0700

    Script to use ffmpeg for preprocessing

---
+++ examples/convert-and-denoise.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Usage:
+#       [source-file] [dest-file]
+
+
+set -xeuo pipefail
+
+echo Denoising $1 -> $2
+
+tmp_wav_in=/tmp/48-raw-single.pcm
+tmp_wav_out=/tmp/48-raw-single-denoised.pcm
+# -ac 1 = mono, s16le is raw
+ffmpeg -y -i $1 -ac 1 -f s16le -c:a pcm_s16le $tmp_wav_in
+./rnnoise_demo $tmp_wav_in $tmp_wav_out
+ffmpeg -y -f s16le -i $tmp_wav_out $2
+
